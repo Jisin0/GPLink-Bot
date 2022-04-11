39a26ed312b355738a047224c4fcf8124bb100fb
Arunkumar Shibu <86429222+arun017s@users.noreply.github.com>
2022-04-11 19:16:48 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,40 @@
+# 11-04-22
+# (C) Arunkumar Shibu
+
+# Using aiogram first time
+# Test
+
+import os
+from aiogram import Bot, Dispatcher, types, executor
+from aiogram.dispatcher.filters import BoundFilter, Command
+from gplink import get_link
+
+BOT_TOKEN = os.environ.get("BOT_TOKEN")
+
+K = Bot(token=BOT_TOKEN)
+bot = Dispatcher(K)
+
+#https://github.com/BotfatherDev/CommonChatModer/blob/work_in_groups/filters/chat_filters.py#L10
+class IsPrivate(BoundFilter):
+    async def check(self, message: types.Message):
+        return message.chat.type == types.ChatType.PRIVATE
+
+
+@bot.message_handler(IsPrivate(), Command("start", prefixes="/"))
+async def start(message):
+    await message.answer("Send gplinlk to get direct link!")
+
+
+@bot.message_handler(IsPrivate())
+async def gp(message):
+    if not message.text.startswith("https://gplinks"):
+       return
+    m = await message.answer("Please wait...")
+    link = get_link(message.text)
+    await m.delete()
+    if not link:      
+       await message.answer("Something went wrong\nTry again later..")
+    else:
+       await message.answer(f"Here is your direct link:\n\n{link}", disable_web_page_preview=True)
+    
+executor.start_polling(bot)
